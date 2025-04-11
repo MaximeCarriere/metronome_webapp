0bad756645e8b5156d4e32ac515a2798d2974eb3
maxime carriere <maximecarriere@maximes-MacBook-Air.local>
2025-04-11 08:57:12 +0200

    add port

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 import plotly.graph_objs as go
 import plotly.offline as pyo
 import random
+import os
 
 app = Flask(__name__)
 
@@ -32,5 +33,8 @@ def index():
 
     return render_template('index.html', plot_div=plot_html)
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
